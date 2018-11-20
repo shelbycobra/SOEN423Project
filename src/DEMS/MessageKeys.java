53ce7b902cbbcd2aff26ad16792b243d57134e1e
Vikram Singh <vsingh7@gmail.com>
2018-11-20 18:30:11 -0500

    Added message keys.

---
+++ src/DEMS/MessageKeys.java
@@ -0,0 +1,17 @@
+package DEMS;
+
+public class MessageKeys
+{
+	public static final String MESSAGE_ID = "message_id";
+	public static final String FIRST_NAME = "first_name";
+	public static final String LAST_NAME = "last_name";
+	public static final String EMPLOYEE_ID = "employee_id";
+	public static final String MAIL_ID = "mail_id";
+	public static final String PROJECTS = "projects";
+	public static final String LOCATION = "location";
+	public static final String MANAGER_ID = "manager_id";
+	public static final String RECORD_ID = "record_id";
+	public static final String FIELD_NAME = "field_name";
+	public static final String NEW_VALUE = "new_value";
+	public static final String REMOTE_SERVER_NAME = "remote_server_name";
+}
