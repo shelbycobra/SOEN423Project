bd16b895d484b36978f4cdf7fe96a16b5700ea1c
Vikram Singh <vsingh7@gmail.com>
2018-11-23 18:42:17 -0500

    Updates Config and Keys for failure types.

--- src/DEMS/Config.java
+++ src/DEMS/Config.java
@@ -38,8 +38,13 @@ public class Config {
 	public static final int CREATE_MANAGER_RECORD = 1, CREATE_EMPLOYEE_RECORD = 2, GET_RECORD_COUNT = 3,
 		EDIT_RECORD = 4, TRANSFER_RECORD = 5, EXIT = 6, ACK = 7;
 
-	public enum statusCode {
+	public enum StatusCode {
 		SUCCESS,
 		FAIL,
+	}
+	
+	public enum Failure {
+	    BYZANTINE,
+	    PROCESS_CRASH,
 	}
 }
