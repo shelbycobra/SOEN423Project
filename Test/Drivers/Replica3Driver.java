057dc30d93b58dcfc91cdb239eaacae9f4241b0b
shelbycobra <s_wams@live.concordia.ca>
2018-11-23 00:22:58 -0500

    Created driver for Replica3

---
+++ Test/Drivers/Replica3Driver.java
@@ -0,0 +1,11 @@
+package Test.Drivers;
+
+import Replicas.Replica3.CenterServerController;
+
+public class Replica3Driver {
+
+    public static void main (String[] args) {
+        CenterServerController servers = new CenterServerController();
+        servers.runServers();
+    }
+}
