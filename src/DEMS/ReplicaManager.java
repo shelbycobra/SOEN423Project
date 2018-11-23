a1ce3bdcaa8582adff6b99e6d77ef8fe7e6f56b9
Francis Bergin <bergin.francis@gmail.com>
2018-11-23 15:35:35 -0500

    Add initial ReplicaManager implementation

--- src/DEMS/ReplicaManager.java
+++ src/DEMS/ReplicaManager.java
@@ -1,4 +1,165 @@
 package DEMS;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class ReplicaManager {
+
+	private final int replicaNumber;
+	private final int replicaManagerPort;
+
+	private final int maxFailedReplicaCount = 3;
+
+	private Thread udpServerThread;
+
+	class UdpServer extends Thread {
+
+		private Map<Integer, Integer> replicaFailureCounts = new HashMap<>();
+		private DatagramSocket datagramSocket;
+		private JSONParser jsonParser = new JSONParser();
+
+		@Override
+		public void run() {
+
+			replicaFailureCounts.put(1, 0);
+			replicaFailureCounts.put(2, 0);
+			replicaFailureCounts.put(3, 0);
+
+			try {
+				datagramSocket = new DatagramSocket(replicaManagerPort);
+			} catch (SocketException e) {
+				e.printStackTrace();
+				return;
+			}
+
+			while (true) {
+
+				byte[] receiveData = new byte[1024];
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+				JSONObject jsonObject;
+
+				try {
+					datagramSocket.receive(receivePacket);
+					jsonObject = processRequest(receivePacket);
+				} catch (IOException | ParseException e) {
+					e.printStackTrace();
+					continue;
+				}
+
+				if (jsonObject.get(MessageKeys.COMMAND_TYPE).equals("REPORT_FAILED_REPLICA")) {
+					int faildReplicaNumber = Integer.parseInt((String) jsonObject.get(MessageKeys.REPLICA_NUMBER));
+
+					if (!((replicaNumber == 1 && (faildReplicaNumber == 2 || faildReplicaNumber == 3)) ||
+							(replicaNumber == 2 && (faildReplicaNumber == 1 || faildReplicaNumber == 3)) ||
+							(replicaNumber == 3 && (faildReplicaNumber == 1 || faildReplicaNumber == 2)))) {
+						notifyFrontEnd("BAD_REPLICA_NUMBER", faildReplicaNumber);
+						continue;
+					}
+
+					int newReplicaFailureCount = replicaFailureCounts.get(faildReplicaNumber) + 1;
+					replicaFailureCounts.put(faildReplicaNumber, newReplicaFailureCount);
+
+					if (newReplicaFailureCount >= maxFailedReplicaCount) {
+						try {
+							restartReplica(faildReplicaNumber);
+						} catch (IOException e) {
+							notifyFrontEnd("FAILED_REPLICA_RESTART_FAILED", faildReplicaNumber);
+							e.printStackTrace();
+							continue;
+						}
+						replicaFailureCounts.put(faildReplicaNumber, 0);
+						notifyFrontEnd("FAILED_REPLICA_RESTARTED", faildReplicaNumber);
+					}
+				}
+
+			}
+		}
+
+		private JSONObject processRequest(DatagramPacket receivePacket) throws ParseException {
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(new String(receivePacket.getData()).trim());
+
+			// send echo to sending client
+			InetAddress IPAddress = receivePacket.getAddress();
+			int port = receivePacket.getPort();
+			byte[] sendDate = jsonObject.toString().getBytes();
+			DatagramPacket datagramPacket = new DatagramPacket(sendDate, sendDate.length, IPAddress, port);
+
+			try {
+				datagramSocket.send(datagramPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return jsonObject;
+		}
+
+		private void restartReplica(int faildReplicaNumber) throws IOException {
+			InetAddress group = InetAddress.getByName("228.5.6.7");
+			MulticastSocket multicastSocket = new MulticastSocket(Config.PortNumbers.SEQ_RE);
+			multicastSocket.joinGroup(group);
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put(MessageKeys.MESSAGE_ID, "RESTART_REPLICA");
+			jsonObject.put(MessageKeys.REPLICA_NUMBER, faildReplicaNumber);
+			byte[] sendDate = jsonObject.toString().getBytes();
+
+			DatagramPacket packet = new DatagramPacket(sendDate, sendDate.length, group, Config.PortNumbers.SEQ_RE);
+			multicastSocket.send(packet);
+
+			multicastSocket.close();
+		}
+
+		private void notifyFrontEnd(String messageID, int replicaNumber) {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put(MessageKeys.MESSAGE_ID, messageID);
+			jsonObject.put(MessageKeys.REPLICA_NUMBER, replicaNumber);
+			byte[] sendDate = jsonObject.toString().getBytes();
+
+			try {
+				InetAddress IPAddress = InetAddress.getByName(Config.FRONT_END_HOST);
+				byte[] sendData = jsonObject.toString().getBytes();
+				DatagramPacket datagramPacket = new DatagramPacket(sendDate, sendDate.length, IPAddress, Config.PortNumbers.SEQ_RE);
+				datagramSocket.send(datagramPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public ReplicaManager(int replicaNumber) {
+		this.replicaNumber = replicaNumber;
+
+		if (replicaNumber == 1) {
+			this.replicaManagerPort = DEMS.Config.Replica1.RM_PORT;
+		} else if (replicaNumber == 2) {
+			this.replicaManagerPort = DEMS.Config.Replica2.RM_PORT;
+		} else if (replicaNumber == 3) {
+			this.replicaManagerPort = DEMS.Config.Replica3.RM_PORT;
+		} else {
+			throw new IllegalArgumentException("Invalid replicaNumber: " + replicaNumber);
+		}
+
+		udpServerThread = new Thread(new UdpServer());
+	}
+
+	public void start() {
+		udpServerThread.start();
+	}
+
+	public void stop() {
+		udpServerThread.interrupt();
+	}
+
 }
