c5f3950a50440ced336309523a92699373a7bce0
Francis Bergin <bergin.francis@gmail.com>
2018-11-25 16:41:19 -0500

    Finish replica manager implementation

--- src/DEMS/ReplicaManager.java
+++ src/DEMS/ReplicaManager.java
@@ -1,5 +1,6 @@
 package DEMS;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -11,10 +12,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ReplicaManager {
+
+	private Replica replica;
 
 	private final int replicaNumber;
 	private final int replicaManagerPort;
@@ -23,27 +24,23 @@ public class ReplicaManager {
 	private final int maxByzantineCount = 3;
 
 	private Thread udpServerThread;
-
 	private Logger logger;
+
+	private InetAddress otherReplicaHost1 = null;
+	private int otherReplicaPort1 = 0;
+	private InetAddress otherReplicaHost2 = null;
+	private int otherReplicaPort2 = 0;
 
 	class UdpServer extends Thread {
 
-		private Map<Integer, Integer> replicaCrashCounts = new HashMap<>();
-		private Map<Integer, Integer> replicaByzantineCounts = new HashMap<>();
+		private int replicaCrashCount = 0;
+		private int replicaByzantineCount = 0;
 
 		private DatagramSocket datagramSocket;
 		private JSONParser jsonParser = new JSONParser();
 
 		@Override
 		public void run() {
-
-			replicaCrashCounts.put(1, 0);
-			replicaCrashCounts.put(2, 0);
-			replicaCrashCounts.put(3, 0);
-
-			replicaByzantineCounts.put(1, 0);
-			replicaByzantineCounts.put(2, 0);
-			replicaByzantineCounts.put(3, 0);
 
 			try {
 				datagramSocket = new DatagramSocket(replicaManagerPort);
@@ -53,7 +50,6 @@ public class ReplicaManager {
 			}
 
 			while (true) {
-
 				byte[] receiveData = new byte[1024];
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
@@ -67,39 +63,40 @@ public class ReplicaManager {
 					continue;
 				}
 
-				int faildReplicaNumber = Integer.parseInt((String) jsonObject.get(MessageKeys.REPLICA_NUMBER));
-				if (!((replicaNumber == 1 && (faildReplicaNumber == 2 || faildReplicaNumber == 3)) ||
-						(replicaNumber == 2 && (faildReplicaNumber == 1 || faildReplicaNumber == 3)) ||
-						(replicaNumber == 3 && (faildReplicaNumber == 1 || faildReplicaNumber == 2)))) {
-					notifyFrontEnd(Config.BAD_REPLICA_NUMBER, faildReplicaNumber);
-					continue;
-				}
+				String commandType = jsonObject.get(MessageKeys.COMMAND_TYPE).toString();
 
-				int replicaCrashCount = replicaCrashCounts.get(faildReplicaNumber);
-				int replicaByzantineCount = replicaByzantineCounts.get(faildReplicaNumber);
-
-				if ((Config.Failure) jsonObject.get(MessageKeys.FAILURE_TYPE) == Config.Failure.PROCESS_CRASH) {
-					replicaCrashCount += 1;
-					replicaCrashCounts.put(faildReplicaNumber, replicaCrashCount);
-				} else if ((Config.Failure) jsonObject.get(MessageKeys.FAILURE_TYPE) == Config.Failure.BYZANTINE) {
-					replicaByzantineCount += 1;
-					replicaByzantineCounts.put(faildReplicaNumber, replicaByzantineCount);
-				}
-
-				if (replicaCrashCount >= maxCrashCount || replicaByzantineCount >= maxByzantineCount) {
-					try {
-						restartReplica(faildReplicaNumber);
-					} catch (IOException e) {
-						notifyFrontEnd(Config.FAILED_REPLICA_RESTART_FAILED, faildReplicaNumber);
-						e.printStackTrace();
-						continue;
+				if (commandType.equals(Config.GET_DATA)) {
+					jsonObject.put(MessageKeys.MESSAGE, replica.getData());
+					jsonObject.put(MessageKeys.STATUS_CODE, Config.StatusCode.SUCCESS.toString());
+				} else if (commandType.equals(Config.REPORT_FAILURE)) {
+					if ((Config.Failure) jsonObject.get(MessageKeys.FAILURE_TYPE) == Config.Failure.PROCESS_CRASH) {
+						replicaCrashCount += 1;
+					} else if ((Config.Failure) jsonObject.get(MessageKeys.FAILURE_TYPE) == Config.Failure.BYZANTINE) {
+						replicaByzantineCount += 1;
 					}
-					replicaCrashCounts.put(faildReplicaNumber, 0);
-					replicaByzantineCounts.put(faildReplicaNumber, 0);
-					notifyFrontEnd(Config.FAILED_REPLICA_RESTARTED, faildReplicaNumber);
+
+					if (replicaCrashCount >= maxCrashCount || replicaByzantineCount >= maxByzantineCount) {
+						try {
+							restartReplica();
+						} catch (Exception e) {
+							e.printStackTrace();
+							jsonObject.put(MessageKeys.MESSAGE, Config.FAILED_REPLICA_RESTART_FAILED);
+							jsonObject.put(MessageKeys.STATUS_CODE, Config.StatusCode.FAIL.toString());
+							continue;
+						}
+						replicaCrashCount = 0;
+						replicaByzantineCount = 0;
+						jsonObject.put(MessageKeys.MESSAGE, Config.REPLICA_RESTARTED);
+						jsonObject.put(MessageKeys.STATUS_CODE, Config.StatusCode.SUCCESS.toString());
+					} else {
+						jsonObject.put(MessageKeys.MESSAGE, Config.FAILURE_COUNTS_INCREMENTED);
+						jsonObject.put(MessageKeys.STATUS_CODE, Config.StatusCode.SUCCESS.toString());
+					}
 				}
 
+				notifyFrontEnd(jsonObject);
 			}
+
 		}
 
 		private JSONObject processRequest(DatagramPacket receivePacket) throws ParseException {
@@ -120,32 +117,31 @@ public class ReplicaManager {
 			return jsonObject;
 		}
 
-		private void restartReplica(int faildReplicaNumber) throws IOException {
-			InetAddress group = InetAddress.getByName("228.5.6.7");
-			MulticastSocket multicastSocket = new MulticastSocket(Config.PortNumbers.SEQ_RE);
-			multicastSocket.joinGroup(group);
+		private void restartReplica() throws Exception {
+			replica.shutdownServers();
+			replica.runServers(0);
 
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put(MessageKeys.COMMAND_TYPE, Config.RESTART_REPLICA);
-			jsonObject.put(MessageKeys.REPLICA_NUMBER, faildReplicaNumber);
-			byte[] sendDate = jsonObject.toString().getBytes();
+			JSONObject jsonSendObject = new JSONObject();
+			jsonSendObject.put(MessageKeys.COMMAND_TYPE, Config.GET_DATA);
+			byte[] sendDate = jsonSendObject.toString().getBytes();
+			DatagramPacket datagramPacket = new DatagramPacket(sendDate, sendDate.length, otherReplicaHost1, otherReplicaPort1);
 
-			DatagramPacket packet = new DatagramPacket(sendDate, sendDate.length, group, Config.PortNumbers.SEQ_RE);
-			multicastSocket.send(packet);
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			datagramSocket.receive(receivePacket); // ignore echo packet
+			datagramSocket.receive(receivePacket);
+			JSONObject jsonReceiveObject = (JSONObject) jsonParser.parse(new String(receivePacket.getData()).trim());
 
-			multicastSocket.close();
+			JSONArray recordData = (JSONArray) jsonReceiveObject.get(MessageKeys.MESSAGE);
+			replica.setData(recordData);
 		}
 
-		private void notifyFrontEnd(String commandType, int replicaNumber) {
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put(MessageKeys.COMMAND_TYPE, commandType);
-			jsonObject.put(MessageKeys.REPLICA_NUMBER, replicaNumber);
-			byte[] sendDate = jsonObject.toString().getBytes();
-
+		private void notifyFrontEnd(JSONObject jsonObject) {
 			try {
-				InetAddress IPAddress = InetAddress.getByName(Config.FRONT_END_HOST);
+				InetAddress frontEndHost = InetAddress.getByName(Config.FRONT_END_HOST);
+				int frontEndPort = Config.PortNumbers.SEQ_RE;
 				byte[] sendData = jsonObject.toString().getBytes();
-				DatagramPacket datagramPacket = new DatagramPacket(sendDate, sendDate.length, IPAddress, Config.PortNumbers.SEQ_RE);
+				DatagramPacket datagramPacket = new DatagramPacket(sendData, sendData.length, frontEndHost, frontEndPort);
 				datagramSocket.send(datagramPacket);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -154,16 +150,29 @@ public class ReplicaManager {
 
 	}
 
-	public ReplicaManager(int replicaNumber) {
+	public ReplicaManager(Replica replica, int replicaNumber) throws UnknownHostException {
+		this.replica = replica;
 		this.replicaNumber = replicaNumber;
 		this.logger = new Logger(replicaNumber);
 
 		if (replicaNumber == 1) {
 			this.replicaManagerPort = DEMS.Config.Replica1.RM_PORT;
+			this.otherReplicaHost1 = InetAddress.getByName(Config.IPAddresses.REPLICA2);
+			this.otherReplicaPort1 = Config.Replica2.RM_PORT;
+			this.otherReplicaHost2 = InetAddress.getByName(Config.IPAddresses.REPLICA3);
+			this.otherReplicaPort2 = Config.Replica3.RM_PORT;
 		} else if (replicaNumber == 2) {
 			this.replicaManagerPort = DEMS.Config.Replica2.RM_PORT;
+			this.otherReplicaHost1 = InetAddress.getByName(Config.IPAddresses.REPLICA1);
+			this.otherReplicaPort1 = Config.Replica1.RM_PORT;
+			this.otherReplicaHost2 = InetAddress.getByName(Config.IPAddresses.REPLICA3);
+			this.otherReplicaPort2 = Config.Replica3.RM_PORT;
 		} else if (replicaNumber == 3) {
 			this.replicaManagerPort = DEMS.Config.Replica3.RM_PORT;
+			this.otherReplicaHost2 = InetAddress.getByName(Config.IPAddresses.REPLICA1);
+			this.otherReplicaPort2 = Config.Replica1.RM_PORT;
+			this.otherReplicaHost1 = InetAddress.getByName(Config.IPAddresses.REPLICA2);
+			this.otherReplicaPort1 = Config.Replica2.RM_PORT;
 		} else {
 			throw new IllegalArgumentException("Invalid replicaNumber: " + replicaNumber);
 		}
@@ -182,7 +191,7 @@ public class ReplicaManager {
 		this.logger.close();
 	}
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws Exception {
 		int replicaNumber = Integer.parseInt(args[0]);
 		int errorType =  Integer.parseInt(args[1]);
 		Class replicaClass = null;
@@ -198,7 +207,7 @@ public class ReplicaManager {
 		}
 
 		Replica replica = (Replica) replicaClass.newInstance();
-		ReplicaManager replicaManager = new ReplicaManager(replicaNumber);
+		ReplicaManager replicaManager = new ReplicaManager(replica, replicaNumber);
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
