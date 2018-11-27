3b237577225915f9ac6cb1bb0deec1c3f44c2b1f
Shelby Wams <s_wams@encs.concordia.ca>
2018-11-26 20:15:04 -0500

    resolved conflict

--- src/FrontEnd/FrontEndImpl.java
+++ src/FrontEnd/FrontEndImpl.java
@@ -331,8 +331,6 @@ public class FrontEndImpl extends FrontEndInterfacePOA
 		{
 			while (elapsedTime < 2 * longestTimeout)
 			{
-				// System.out.println("Received " + message.getReturnMessages().size() + " messages so far...");
-				
 				if (message.getReturnMessages().size() == 3)
 				{
 					System.out.println("Got 3 messages!");
